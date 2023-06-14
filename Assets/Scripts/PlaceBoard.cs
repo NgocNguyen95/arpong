3dab73e59fcbc5c5f0102113e30cb44008fdd777
NgocNguyen95 <31845250+NgocNguyen95@users.noreply.github.com>
2023-06-14 23:13:56 +0300

    Fixed AR board rotates randomly
    Also hide the board when ray cast hits vertical AR planes

--- Assets/Scripts/PlaceBoard.cs
+++ Assets/Scripts/PlaceBoard.cs
@@ -7,14 +7,43 @@ public class PlaceBoard : MonoBehaviour
 {
     public GameObject cursor;
     public ARRaycastManager arRaycastManager;
+    public ARPlaneManager arPlaneManager;
+
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private ARPlane _plane;
+
+
+    private void OnEnable()
+    {
+        _plane = null;
+    }
+
+
+    private void OnDisable()
+    {
+        _plane = null;
+    }
+
 
     void Update()
     {
         // Update cursor position
         Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
-        if (arRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes))
+        if (arRaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon))
         {
+            _plane = arPlaneManager.GetPlane(hits[0].trackableId);
+            if (_plane == null)
+            {
+                cursor.SetActive(false);
+                return;
+            }
+
+            if (_plane.alignment != PlaneAlignment.HorizontalUp)
+            {
+                cursor.SetActive(false);
+                return;
+            }
+
             Pose hitPose = hits[0].pose;
             cursor.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
             cursor.SetActive(true);
